6d4cdc121168dfc9ba1e8ce8c48beb7e4f6bc8ec
Nate Densmore <ntdens@gmail.com>
2024-05-07 00:17:28 -0400

    scrollbar size

--- pages/events.py
+++ pages/events.py
@@ -45,7 +45,35 @@ if not firebase_admin._apps:
     })
 
 with open( "style.css" ) as css:
-    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+    st.markdown( f'<style>{css.read()}</style>' + """
+                <html>
+                    <head>
+                    <style>
+                        ::-webkit-scrollbar {
+                            width: 10px;
+                            height: 10px;
+                            }
+
+                            /* Track */
+                            ::-webkit-scrollbar-track {
+                            background: #f1f1f1;
+                            }
+
+                            /* Handle */
+                            ::-webkit-scrollbar-thumb {
+                            background: #888;
+                            }
+
+                            /* Handle on hover */
+                            ::-webkit-scrollbar-thumb:hover {
+                            background: #555;
+                            }
+                    </style>
+                    </head>
+                    <body>
+                    </body>
+                </html>
+            """, unsafe_allow_html= True)
 
 with open('./config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
