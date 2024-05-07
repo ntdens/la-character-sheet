5eb426e5edbb91a96c13099e81c69193d964c15a
Nate Densmore <ntdens@gmail.com>
2024-05-06 22:54:01 -0400

    admin zone update

--- pages/admin_zone.py
+++ pages/admin_zone.py
@@ -128,10 +128,13 @@ if st.session_state["authentication_status"]:
                 known = ast.literal_eval(character_data['known'])
                 known_data = df[df['Skill Name'].isin(known)]
                 display_data = known_data[['Skill Name', 'Description', 'Limitations', 'Prerequisite']].drop_duplicates(subset=['Skill Name']).copy()
-                image_location = character_data['pic_name']
-                bucket = storage.bucket()
-                blob = bucket.blob(image_location)
-                profile_image = blob.download_as_bytes()
+                try:
+                    image_location = character_data['pic_name']
+                    bucket = storage.bucket()
+                    blob = bucket.blob(image_location)
+                    profile_image = blob.download_as_bytes()
+                except:
+                    profile_image = "https://static.wixstatic.com/media/e524a6_cb4ccb346db54d2d9b00dbaee7610a97~mv2.png/v1/crop/x_0,y_3,w_800,h_795/fill/w_160,h_153,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/e524a6_cb4ccb346db54d2d9b00dbaee7610a97~mv2.png"
                 with st.container(border=True):
                     my_grid = grid([4,6],1)
                     my_grid.container(border=True).image(profile_image)
@@ -154,97 +157,6 @@ if st.session_state["authentication_status"]:
                 st.info("Data does not exist for this user")
     else:
         st.warning('Not an admin. Access denied. Whomp whomp.')
-
-
-    # player = st.session_state["name"]
-
-
-    # with tab2:
-    #     with st.form('my_form'):
-    #         character_name_input = st.text_input('Character Name', value=character_name, key='form_char')
-    #         path_input = st.selectbox('Path', path_list, index=path_list.index(path), key='form_path')
-    #         faction_input = st.selectbox('Faction', faction_list, index=faction_list.index(faction), key='form_faction')
-    #         uploaded_file = st.file_uploader('Upload Profile Picture', type=['png','gif','jpg','jpeg'], key='form_image')
-    #         if uploaded_file is not None:
-    #             form_image = uploaded_file.getvalue()
-    #             pic_name = '{}.{}'.format(st.session_state['username'],uploaded_file.name.split('.')[1])
-    #             image = Image.open(io.BytesIO(form_image))
-    #             image.save(pic_name)
-    #         submitted = st.form_submit_button('Save Edits')
-    #         if submitted:
-    #             doc_ref = db.reference("users/").child(st.session_state['username'])
-    #             doc_ref.update({
-    #                 "character_name":character_name_input,
-    #                 "path":path_input,
-    #                 "faction":faction_input,
-    #             })
-    #             if uploaded_file is not None:
-    #                 doc_ref.update({
-    #                 "pic_name":pic_name
-    #                 })
-    #                 bucket = storage.bucket()
-    #                 blob = bucket.blob(pic_name)
-    #                 blob.upload_from_filename(pic_name)
-    #                 os.remove(pic_name)
-
-    # if 'form_char' in st.session_state:
-    #     character_name = st.session_state['form_char']
-    # if 'form_path' in st.session_state:
-    #     path = st.session_state['form_path']
-    # if 'form_faction' in st.session_state:
-    #     faction = st.session_state['form_faction']
-    # if uploaded_file is not None:
-    #     profile_image = st.session_state['form_image']
-
-
-    # with tab3:
-    #     df = pd.read_excel('Skills_Table.xlsx')
-    #     df['Tier'] = df.Tier.astype(int)
-    #     skill_path = path[2:]
-    #     point_cost = []
-    #     for index, row in df.iterrows():
-    #         if row['Path'] != skill_path:
-    #             if row['Path'] != 'Bard':
-    #                 if row['Tier'] == 0:
-    #                     point_cost.append(2)
-    #                 else:
-    #                     point_cost.append(row['Tier']*2)
-    #             else:
-    #                 point_cost.append(row['Tier'])
-    #         else:
-    #             point_cost.append(row['Tier'])
-    #     df['Point Cost'] = point_cost
-    #     df, display_df = skill_gain(df, skill_path, tier)
-    #     "## Known Skills"
-    #     st.dataframe(display_df, hide_index=True, use_container_width=True)
-    #     "## Available Skills"
-    #     st.dataframe(df, hide_index=True, use_container_width=True)
-
-    
-
-
-    # with tab1:
-    #     df = pd.read_excel('Skills_Table.xlsx')
-    #     known = st.session_state['known']
-    #     known_data = df[df['Skill Name'].isin(known)]
-    #     display_data = known_data[['Skill Name', 'Description', 'Limitations', 'Prerequisite']].drop_duplicates(subset=['Skill Name']).copy()
-    #     points_available = skill_points - st.session_state['point_spend']
-    #     with st.container(border=True):
-    #         my_grid = grid([4,6],1)
-    #         my_grid.container(border=True).image(profile_image)
-    #         player_data = pd.DataFrame({
-    #             'Category': ['Character: ','Player: ','Path: ','Faction: ','Tier: ','Skill Points: '],
-    #             'Information': [character_name,player,path,faction,tier,points_available]
-    #                             })
-    #         my_grid.dataframe(player_data, hide_index=True, use_container_width=True)
-    #         my_grid.dataframe(display_data.astype(str), hide_index=True, use_container_width=True)
-
-
-
-
-
-
-    
 
 elif st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
