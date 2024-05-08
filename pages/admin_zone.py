40756de14e6a33f89c2ad0979e9c4ecf97eb6b2e
Nate Densmore <ntdens@gmail.com>
2024-05-08 00:49:08 -0400

    Update admin_zone.py

--- pages/admin_zone.py
+++ pages/admin_zone.py
@@ -221,6 +221,7 @@ if st.session_state["authentication_status"]:
             character_choice = st.selectbox('Select User:', user_df['Username'], key='sheet_user', index=list(user_df['Username']).index(st.session_state['username']))
             try:
                 character_data = user_data[character_choice]
+                st.dataframe(character_data)
                 known = ast.literal_eval(character_data['known'])
                 known_data = df[df['Skill Name'].isin(known)]
                 display_data = known_data[['Skill Name', 'Description', 'Limitations', 'Prerequisite']].drop_duplicates(subset=['Skill Name']).copy()
