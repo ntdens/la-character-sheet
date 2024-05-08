824a8d5ad5c6dae96fd32edfe54abb00e5808695
Nate Densmore <ntdens@gmail.com>
2024-05-08 13:41:52 -0400

    Update admin_zone.py

--- pages/admin_zone.py
+++ pages/admin_zone.py
@@ -220,8 +220,23 @@ if st.session_state["authentication_status"]:
             user_df = filter_dataframe(user_df)
             st.dataframe(user_df, hide_index=True)
             tier_df = user_df.groupby('Tier')['Username'].count().reset_index().rename(columns={'Username':'Players'})
+            path_df = user_df.groupby('Path')['Username'].count().reset_index().rename(columns={'Username':'Players'})
             st.plotly_chart(
                 px.bar(tier_df, x='Tier', y='Players', title='Number of Players by Tier').update_layout(
+                    xaxis = dict(
+                        tickmode = 'array',
+                        tickvals = path_list,
+                        ticktext = path_list
+                    ),
+                    yaxis = dict(
+                            tickmode = 'linear',
+                            tick0 = 0,
+                            dtick = 1
+                    )
+                ).update_traces(marker_color='rgb(230,171,2)')
+            )
+            st.plotly_chart(
+                px.bar(tier_df, x='Path', y='Players', title='Number of Players by Path').update_layout(
                     xaxis = dict(
                         tickmode = 'array',
                         tickvals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
