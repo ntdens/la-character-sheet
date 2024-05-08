ecd073ac3cdc74dfb495e28420c104c4edcde126
Nate Densmore <ntdens@gmail.com>
2024-05-08 13:56:09 -0400

    Update admin_zone.py

--- pages/admin_zone.py
+++ pages/admin_zone.py
@@ -174,7 +174,7 @@ authenticator.login()
 
 #authenticate users
 if st.session_state["authentication_status"]:
-    if st.session_state['username'] in (st.secrets['admins'] or list(st.secrets['faction_leaders'])):
+    if st.session_state['username'] in (st.secrets['admins'] + list(st.secrets['faction_leaders'])):
         if st.session_state['username'] in list(st.secrets['faction_leaders']):
             faction_filter = st.secrets['faction_leaders'][st.session_state['username']]
         else:
