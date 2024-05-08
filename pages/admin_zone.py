5507a7f9dab2207e2f800f3ec7910478d0873ec8
Nate Densmore <ntdens@gmail.com>
2024-05-08 09:53:17 -0400

    Update admin_zone.py

--- pages/admin_zone.py
+++ pages/admin_zone.py
@@ -12,6 +12,10 @@ import ast
 import smtplib
 from email.mime.text import MIMEText
 import plotly.express as px
+from pandas.api.types import (
+    is_datetime64_any_dtype,
+    is_numeric_dtype,
+)
 
 add_page_title(layout='wide')
 
@@ -71,6 +75,68 @@ path_list = [
 
 def get_tier(events):
     return floor((sqrt(8*events)-1)/2)
+
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
+
+    Args:
+        df (pd.DataFrame): Original dataframe
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+    modify = st.checkbox("Add filters")
+
+    if not modify:
+        return df
+
+    df = df.copy()
+    df = df.fillna('None')
+    modification_container = st.container()
+
+    with modification_container:
+        for column in df.columns:
+            left, right = st.columns((1, 20))
+            if is_numeric_dtype(df[column]):
+                _min = df[column].min()
+                _max = df[column].max()
+                user_num_input = right.slider(
+                    f"Values for {column}",
+                    min_value=_min,
+                    max_value=_max,
+                    value=(_min, _max),
+                    step=1,
+                )
+                df = df[df[column].between(*user_num_input)]
+            # Treat columns with < 10 unique values as categorical
+            elif isinstance(df[column], pd.CategoricalDtype) or df[column].nunique() < 10:
+                user_cat_input = right.multiselect(
+                    f"Values for {column}",
+                    df[column].unique(),
+                    default=list(df[column].unique()),
+                )
+                df = df[df[column].isin(user_cat_input)]
+            elif is_datetime64_any_dtype(df[column]):
+                user_date_input = right.date_input(
+                    f"Values for {column}",
+                    value=(
+                        df[column].min(),
+                        df[column].max(),
+                    ),
+                )
+                if len(user_date_input) == 2:
+                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
+                    start_date, end_date = user_date_input
+                    df = df.loc[df[column].between(start_date, end_date)]
+            else:
+                user_text_input = right.text_input(
+                    f"Substring or regex in {column}",
+                )
+                if user_text_input:
+                    df = df[df[column].astype(str).str.contains(user_text_input)]
+
+    return df
 
 if not firebase_admin._apps:
     key_dict = json.loads(st.secrets["firebase"], strict=False)
@@ -134,6 +200,7 @@ if st.session_state["authentication_status"]:
         user_df = pd.DataFrame(user_table)
         if faction_filter != None:
             user_df = user_df[user_df['Faction'] == faction_filter]
+        user_df = filter_dataframe(user_df)
         with tab1:
             leader_data = user_df[user_df['Username'] == st.session_state['username']]
             st.write("## Welcome {}, Leader of {}".format(leader_data['Character'].values[0],leader_data['Faction'].values[0]))
